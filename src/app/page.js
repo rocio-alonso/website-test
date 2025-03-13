660108b9157c3244f6553f253f38a467711eab45
JulianLipa <lipajulianp@gmail.com>
2025-03-13 12:21:57 -0300

    images test v1.2

--- src/app/page.js
+++ src/app/page.js
@@ -5,17 +5,21 @@ import Gallery from "./components/lib/Gallery/Gallery";
 
 const page = () => {
   return (
-    <div className="hero">
-      <CldImage
-        src="bollini-2023/ucsijhy1cqs3hcew6rqe"
-        width="500"
-        height="500"
-        alt="image"
-        crop="fill"
-      />
-
-      <Gallery></Gallery>
-    </div>
+    <section className="">
+      <div className="hero bg-blue-700">
+        <CldImage
+          src="bollini-2023/ucsijhy1cqs3hcew6rqe"
+          width="500"
+          height="500"
+          alt="image"
+          crop="fill"
+        />
+      </div>
+      <div className="hero bg-amber-50">
+        <h1>test</h1>
+        <Gallery></Gallery>
+      </div>
+    </section>
   );
 };
 
